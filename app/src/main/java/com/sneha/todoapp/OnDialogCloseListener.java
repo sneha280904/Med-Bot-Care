c072585f38a7ca22f2846feaaae25811f6843b6e
RCCS <sneha280904@gmail.com>
2023-04-02 11:51:26 +0530

    Final Commit

---
+++ app/src/main/java/com/sneha/todoapp/OnDialogCloseListener.java
@@ -0,0 +1,8 @@
+package com.sneha.todoapp;
+
+import android.content.DialogInterface;
+
+public interface OnDialogCloseListener {
+
+    void onDialogClose(DialogInterface dialogInterface);
+}
---
+++ app/src/main/java/com/sneha/todoapp/RecyclerViewTouchHelper.java
@@ -0,0 +1,77 @@
+package com.sneha.todoapp;
+
+import static androidx.recyclerview.widget.ItemTouchHelper.*;
+
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+
+import androidx.annotation.NonNull;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.sneha.todoapp.Adapter.ToDoAdapter;
+
+public class RecyclerViewTouchHelper extends Callback {
+    private final ToDoAdapter adapter;
+
+    public RecyclerViewTouchHelper(ToDoAdapter adapter) {
+        super(0, LEFT | RIGHT);
+        this.adapter = adapter;
+
+    }
+
+    @Override
+    public int getMovementFlags(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder) {
+        return 0;
+    }
+
+    @Override
+    public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+        return false;
+    }
+
+    @Override
+    public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+        final int position = viewHolder.getAdapterPosition();
+        if (direction == RIGHT){
+            AlertDialog.Builder builder = new AlertDialog.Builder(adapter.getContext());
+            builder.setTitle("Delete Task");
+            builder.setMessage("Are You Sure ?");;
+            builder.setPositiveButton("Yes", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which){
+                    adapter.deleteTask(position);
+                }
+            });
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener(){
+                @Override
+                public void onClick(DialogInterface dialog, int which){
+                    adapter.notifyItemChanged(position);
+                }
+            });
+            AlertDialog dialog = builder.create();
+            dialog.show();
+        }
+        else{
+            adapter.editItem(position);
+        }
+    }
+
+    @Override
+    public void onChildDraw(@NonNull Canvas c, @NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder,float dX, float dY, int actionState, boolean isCurrentlyActive){
+
+        new RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+                .addSwipeLeftBackgroundColor(ContextCompat.getColor(adapter.getContext(), R.color.colorPrimaryDark))
+                .addSwipeLeftActionIcon(R.drawable.baseline_edit)
+                .addSwipeRightBackgroundColor(Color.RED)
+                .addSwipeRightActionIcon(R.drawable.baseline_delete)
+                .create()
+                .decorate();
+        super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive);
+
+    }
+
+}
